47d7d88c3a941da439989fff9c381d67d19c3789
JungHoiMin <wony1102@naver.com>
2022-05-24 13:17:24 +0900

    사진 여러장 촬영 할 수 있도록 업데이트

--- python/save_user.py
+++ python/save_user.py
@@ -31,12 +31,12 @@ def take_pictures():
     while True:
         ret, frame = cap.read()
         if face_extractor(frame) is not None:
-            break
+            count+=1
 
             face = cv2.resize(face_extractor(frame),(200,200))
             face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
 
-            file_name_path = face_dirs+'picture.jpg'
+            file_name_path = face_dirs+'picture' + str(count) + '.jpg'
             cv2.imwrite(file_name_path,face)
 
             cv2.putText(face,str(count),(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
@@ -44,6 +44,9 @@ def take_pictures():
         else:
             print("Face not Found. Current count is {0}".format(count))
             pass
+
+        if (cv2.waitKey(1) & 0xff) == 27 or count == 100:
+            break
 
     cap.release()
     cv2.destroyAllWindows()
