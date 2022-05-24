175a5da37ca551cb0c3e06d22cb4885b8c36078e
JungHoiMin <wony1102@naver.com>
2022-05-24 13:14:33 +0900

    사진 촬영 함수 추가(테스트)

---
+++ python/save_user.py
@@ -0,0 +1,54 @@
+import cv2
+import numpy as np
+from os import makedirs
+from os.path import isdir
+
+face_dirs = 'faces/'
+if not isdir(face_dirs):
+    makedirs(face_dirs)
+
+face_classifier = cv2.CascadeClassifier('../openCV/haarcascade_frontalface_default.xml')
+
+def face_extractor(img):
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    faces = face_classifier.detectMultiScale(gray,1.3,5)
+    if faces is():
+        return None
+
+    for(x,y,w,h) in faces:
+        cropped_face = img[y:y+h, x:x+w]
+
+    return cropped_face
+
+
+def take_pictures():
+    if not isdir(face_dirs+name):
+        makedirs(face_dirs+name)
+
+    cap = cv2.VideoCapture(0)
+    count = 0
+
+    while True:
+        ret, frame = cap.read()
+        if face_extractor(frame) is not None:
+            break
+
+            face = cv2.resize(face_extractor(frame),(200,200))
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+
+            file_name_path = face_dirs+'picture.jpg'
+            cv2.imwrite(file_name_path,face)
+
+            cv2.putText(face,str(count),(50,50),cv2.FONT_HERSHEY_COMPLEX,1,(0,255,0),2)
+            cv2.imshow('Face Cropper',face)
+        else:
+            print("Face not Found. Current count is {0}".format(count))
+            pass
+
+    cap.release()
+    cv2.destroyAllWindows()
+    print('Colleting Samples Complete!!!')
+
+
+if __name__=="__main__":
+    take_pictures()
